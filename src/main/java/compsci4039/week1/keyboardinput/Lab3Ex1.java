9f8dc00a18bd232a74791cfc5cbb1473d868113e
Pedro Belfort <belfortdev@gmail.com>
2019-09-24 12:22:23 +0100

    Setup project

---
+++ src/main/java/compsci4039/week1/keyboardinput/Lab3Ex1.java
@@ -0,0 +1,9 @@
+package compsci4039.week1.keyboardinput;
+
+public class Lab3Ex1 {
+
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/test/java/compsci4039/week1/keyboardinput/Lab3Ex1Tests.java
@@ -0,0 +1,16 @@
+package compsci4039.week1.keyboardinput;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Lab3Ex1Tests {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        int expected = 5;
+        int actual = 5;
+        assertEquals(expected, actual);
+    }
+
+
+}
